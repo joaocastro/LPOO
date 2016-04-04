95ed594fc21025de508dba7e1836865b24c51c31
joaocastro <up201404896@fe.up.pt>
2016-04-04 14:54:31 +0100

    fritando no algoritmo

---
+++ src/maze/gui/ManualMaze.java
@@ -0,0 +1,43 @@
+package maze.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class ManualMaze {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ManualMaze window = new ManualMaze();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ManualMaze() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
